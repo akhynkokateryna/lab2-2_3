6726f41518dcd553d2c24127863008c7f6f18fa9
Kateryna Akhynko <kateryna.akhynko@ucu.edu.ua>
2022-02-18 22:55:18 +0200

    written app

--- app_flask.py
+++ app_flask.py
@@ -1,1 +1,14 @@
+"app_flask.py"
+
 from flask import Flask, render_template, request
+import web_navigator
+
+app = Flask(__name__)
+
+@app.route("/redirecting", methods = ["POST"])
+def redirecting():
+    if request.method == "POST":
+        data = request.form
+        nickname = data["name"]
+        web_navigator.main(nickname)
+        return render_template('new_map.html')
