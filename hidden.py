1d7563c68a1466f4b79009431355ed4ef5dcdc44
Kateryna Akhynko <kateryna.akhynko@ucu.edu.ua>
2022-02-18 18:20:30 +0200

    commit

---
+++ hidden.py
@@ -0,0 +1,5 @@
+def oauth():
+    return {"consumer_key": "gIajVemp7qgnsSD2PKShcytPp",
+            "consumer_secret": "0x3M1u8Qf7ChY7KWTL0gSeiAdaMkBiAkxp5BiRmYTsw6Uyr665",
+            "token_key": "1483744436215595010-MaoYUtoADhTfq31qohPusVguwH3ZcE",
+            "token_secret": "63AfifsliBL25RvXLeyksGZZ2RhXWeRk9D6XJoIEh0Bzl"}
